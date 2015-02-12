4f47b87d5a7eda242fa70446cd9fcff158a94a1a
sleach2 <sleach2@gmu.edu>
2015-02-12 02:10:49 -0500

    u

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ window.onload = function() {
         ledge.body.immovable = true;
         ledge = platforms.create(-130, 250, 'ground');
         ledge.body.immovable = true;
-        ledge = platforms.create(500, 20, 'ground');
+        ledge = platforms.create(700, 20, 'ground');
         ledge.body.immovable = true;
     }
     
