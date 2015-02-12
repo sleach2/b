d287b8168ece4c7fdd61e4d406679d0f928477c4
sleach2 <sleach2@gmu.edu>
2015-02-11 19:20:42 -0500

    F

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ window.onload = function() {
 
     function preload() {
         game.load.image( 'logo', 'assets/phaser.png' );
-        game.load.spritesheet('cat','assets/cat1.png', 40, 50);
+        game.load.spritesheet('cat','assets/cat1.png', 45, 55);
         game.load.image('ground', 'assets/platform.png');
         game.load.image('sky', 'assets/sky.png');
     }
