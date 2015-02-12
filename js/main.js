fe52bd863a22dbe803f07a1d8da3c605c6bba127
sleach2 <sleach2@gmu.edu>
2015-02-11 19:45:41 -0500

    f

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ window.onload = function() {
         player.body.bounce.y = 0.2;
         player.body.gravity.y = 300;
         player.body.collideWorldBounds = true;
-        player.body.setSize(20, 32, 5, 16);
+        //player.body.setSize(20, 32, 5, 16);
     }
     
     function update() {
