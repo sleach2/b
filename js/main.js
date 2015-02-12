139aa9471f6858667fd277bf1b96abaa67ab42b8
sleach2 <sleach2@gmu.edu>
2015-02-11 19:52:54 -0500

    t

--- js/main.js
+++ js/main.js
@@ -36,17 +36,17 @@ window.onload = function() {
     function update() {
         game.physics.arcade.collide(player, platforms);
         player.body.velocity.x = 0;
-        if (cursors.left.isDown){ 
+        if (move.left.isDown){ 
              player.body.velocity.x = -250; 
              player.animations.play('left'); 
-        }else if (cursors.right.isDown){ 
+        }else if (move.right.isDown){ 
              player.body.velocity.x = 250; 
              player.animations.play('right'); 
          }else{ 
              player.animations.stop(); 
              player.frame = 0; 
          } 
-         if (cursors.up.isDown && player.body.touching.down){ 
+         if (move.up.isDown && player.body.touching.down){ 
             player.body.velocity.y = -350; 
         } 
 
