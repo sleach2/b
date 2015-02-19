52bc68ca4c5efaf1db6f97c8c1b3f04ea1c6148d
sleach2 <sleach2@gmu.edu>
2015-02-19 00:40:10 -0500

    f

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@
         }
     }
 };*/
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
